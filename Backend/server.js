622a3afa01351b922a844a9e5dfcff7bc942d131
Md. Ratul Mushfique <md.ratul.mushfique@g.bracu.ac.bd>
2025-08-16 04:19:10 +0600

    Lessen CORS for render deployment

--- Backend/server.js
+++ Backend/server.js
@@ -11,10 +11,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
+app.use(cors());
+///app.use(cors({
+///    origin: 'http://localhost:5173',
+///    credentials: true
+///}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
